60a47c1a7a6b8fec343ef5696b96dc51bd30590d
Roman Pelikh <pelikh.roman@gmail.com>
2019-04-30 12:13:23 -0300

    finished

--- SortedListExTask/Form1.Designer.cs
+++ SortedListExTask/Form1.Designer.cs
@@ -28,101 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnRemoveTask = new System.Windows.Forms.Button();
-            this.lstTasks = new System.Windows.Forms.ListBox();
-            this.btnPrintAll = new System.Windows.Forms.Button();
-            this.lblTaskDetails = new System.Windows.Forms.Label();
-            this.btnAddTask = new System.Windows.Forms.Button();
-            this.txtTask = new System.Windows.Forms.TextBox();
-            this.dtpTaskDate = new System.Windows.Forms.DateTimePicker();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(237, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 17);
-            this.label1.TabIndex = 16;
-            this.label1.Text = "Task List";
-            // 
-            // btnRemoveTask
-            // 
-            this.btnRemoveTask.Location = new System.Drawing.Point(434, 33);
-            this.btnRemoveTask.Name = "btnRemoveTask";
-            this.btnRemoveTask.Size = new System.Drawing.Size(134, 23);
-            this.btnRemoveTask.TabIndex = 15;
-            this.btnRemoveTask.Text = "Remove Task";
-            this.btnRemoveTask.UseVisualStyleBackColor = true;
-            // 
-            // lstTasks
-            // 
-            this.lstTasks.FormattingEnabled = true;
-            this.lstTasks.Location = new System.Drawing.Point(240, 33);
-            this.lstTasks.Name = "lstTasks";
-            this.lstTasks.Size = new System.Drawing.Size(188, 95);
-            this.lstTasks.TabIndex = 14;
-            // 
-            // btnPrintAll
-            // 
-            this.btnPrintAll.Location = new System.Drawing.Point(434, 59);
-            this.btnPrintAll.Name = "btnPrintAll";
-            this.btnPrintAll.Size = new System.Drawing.Size(134, 23);
-            this.btnPrintAll.TabIndex = 13;
-            this.btnPrintAll.Text = "Print All Tasks";
-            this.btnPrintAll.UseVisualStyleBackColor = true;
-            // 
-            // lblTaskDetails
-            // 
-            this.lblTaskDetails.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblTaskDetails.Location = new System.Drawing.Point(240, 131);
-            this.lblTaskDetails.Name = "lblTaskDetails";
-            this.lblTaskDetails.Size = new System.Drawing.Size(188, 57);
-            this.lblTaskDetails.TabIndex = 12;
-            // 
-            // btnAddTask
-            // 
-            this.btnAddTask.Location = new System.Drawing.Point(24, 165);
-            this.btnAddTask.Name = "btnAddTask";
-            this.btnAddTask.Size = new System.Drawing.Size(200, 23);
-            this.btnAddTask.TabIndex = 11;
-            this.btnAddTask.Text = "Add Task";
-            this.btnAddTask.UseVisualStyleBackColor = true;
-            // 
-            // txtTask
-            // 
-            this.txtTask.Location = new System.Drawing.Point(24, 59);
-            this.txtTask.Multiline = true;
-            this.txtTask.Name = "txtTask";
-            this.txtTask.Size = new System.Drawing.Size(200, 100);
-            this.txtTask.TabIndex = 10;
-            // 
-            // dtpTaskDate
-            // 
-            this.dtpTaskDate.Location = new System.Drawing.Point(24, 33);
-            this.dtpTaskDate.Name = "dtpTaskDate";
-            this.dtpTaskDate.Size = new System.Drawing.Size(200, 20);
-            this.dtpTaskDate.TabIndex = 9;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 200);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnRemoveTask);
-            this.Controls.Add(this.lstTasks);
-            this.Controls.Add(this.btnPrintAll);
-            this.Controls.Add(this.lblTaskDetails);
-            this.Controls.Add(this.btnAddTask);
-            this.Controls.Add(this.txtTask);
-            this.Controls.Add(this.dtpTaskDate);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.label1 = new System.Windows.Forms.Label();
+      this.btnRemoveTask = new System.Windows.Forms.Button();
+      this.lstTasks = new System.Windows.Forms.ListBox();
+      this.btnPrintAll = new System.Windows.Forms.Button();
+      this.lblTaskDetails = new System.Windows.Forms.Label();
+      this.btnAddTask = new System.Windows.Forms.Button();
+      this.txtTask = new System.Windows.Forms.TextBox();
+      this.dtpTaskDate = new System.Windows.Forms.DateTimePicker();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(316, 16);
+      this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(87, 20);
+      this.label1.TabIndex = 16;
+      this.label1.Text = "Task List";
+      // 
+      // btnRemoveTask
+      // 
+      this.btnRemoveTask.Location = new System.Drawing.Point(579, 41);
+      this.btnRemoveTask.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.btnRemoveTask.Name = "btnRemoveTask";
+      this.btnRemoveTask.Size = new System.Drawing.Size(179, 28);
+      this.btnRemoveTask.TabIndex = 15;
+      this.btnRemoveTask.Text = "Remove Task";
+      this.btnRemoveTask.UseVisualStyleBackColor = true;
+      this.btnRemoveTask.Click += new System.EventHandler(this.btnRemoveTask_Click);
+      // 
+      // lstTasks
+      // 
+      this.lstTasks.FormattingEnabled = true;
+      this.lstTasks.ItemHeight = 16;
+      this.lstTasks.Location = new System.Drawing.Point(320, 41);
+      this.lstTasks.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.lstTasks.Name = "lstTasks";
+      this.lstTasks.Size = new System.Drawing.Size(249, 116);
+      this.lstTasks.TabIndex = 14;
+      this.lstTasks.SelectedValueChanged += new System.EventHandler(this.lstTasks_SelectedValueChanged);
+      // 
+      // btnPrintAll
+      // 
+      this.btnPrintAll.Location = new System.Drawing.Point(579, 73);
+      this.btnPrintAll.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.btnPrintAll.Name = "btnPrintAll";
+      this.btnPrintAll.Size = new System.Drawing.Size(179, 28);
+      this.btnPrintAll.TabIndex = 13;
+      this.btnPrintAll.Text = "Print All Tasks";
+      this.btnPrintAll.UseVisualStyleBackColor = true;
+      this.btnPrintAll.Click += new System.EventHandler(this.btnPrintAll_Click);
+      // 
+      // lblTaskDetails
+      // 
+      this.lblTaskDetails.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.lblTaskDetails.Location = new System.Drawing.Point(320, 161);
+      this.lblTaskDetails.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lblTaskDetails.Name = "lblTaskDetails";
+      this.lblTaskDetails.Size = new System.Drawing.Size(251, 70);
+      this.lblTaskDetails.TabIndex = 12;
+      // 
+      // btnAddTask
+      // 
+      this.btnAddTask.Location = new System.Drawing.Point(32, 203);
+      this.btnAddTask.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.btnAddTask.Name = "btnAddTask";
+      this.btnAddTask.Size = new System.Drawing.Size(267, 28);
+      this.btnAddTask.TabIndex = 11;
+      this.btnAddTask.Text = "Add Task";
+      this.btnAddTask.UseVisualStyleBackColor = true;
+      this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+      // 
+      // txtTask
+      // 
+      this.txtTask.Location = new System.Drawing.Point(32, 73);
+      this.txtTask.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.txtTask.Multiline = true;
+      this.txtTask.Name = "txtTask";
+      this.txtTask.Size = new System.Drawing.Size(265, 122);
+      this.txtTask.TabIndex = 10;
+      // 
+      // dtpTaskDate
+      // 
+      this.dtpTaskDate.Location = new System.Drawing.Point(32, 41);
+      this.dtpTaskDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.dtpTaskDate.Name = "dtpTaskDate";
+      this.dtpTaskDate.Size = new System.Drawing.Size(265, 22);
+      this.dtpTaskDate.TabIndex = 9;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(789, 246);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.btnRemoveTask);
+      this.Controls.Add(this.lstTasks);
+      this.Controls.Add(this.btnPrintAll);
+      this.Controls.Add(this.lblTaskDetails);
+      this.Controls.Add(this.btnAddTask);
+      this.Controls.Add(this.txtTask);
+      this.Controls.Add(this.dtpTaskDate);
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
--- SortedListExTask/Form1.cs
+++ SortedListExTask/Form1.cs
@@ -16,5 +16,112 @@ namespace SortedListExTask
         {
             InitializeComponent();
         }
+
+    Dictionary<string, string> myTasks = new Dictionary<string, string>();
+
+
+    private void TaskSelectedDisplay( )
+    {
+      if(lstTasks.SelectedIndex != -1)
+      {
+        //string key = lstTasks.SelectedItem.ToString();
+        // lblTaskDetails.Text = myTasks [key];
+
+        string val = myTasks[lstTasks.SelectedItem.ToString()];
+        lblTaskDetails.Text = val;
+      }
+      else
+      {
+        lblTaskDetails.ResetText();
+      } 
     }
+
+    private void RemoveTask(string key )
+    {
+      myTasks.Remove(key);   
+      lstTasks.Items.Remove( lstTasks.SelectedItem );
+    }
+
+    private void AddTask(string task, Dictionary <string, string> taskCollection )
+    {
+      taskCollection.Add( dtpTaskDate.Value.ToShortDateString(), task );
+      DisplayTaskInList();
+      txtTask.Clear();
+    }
+
+    private void DisplayTaskInList( )
+    {      
+        lstTasks.Items.Add( myTasks.Keys.ElementAt(myTasks.Count-1));   
+    }
+
+    private void btnAddTask_Click( object sender, EventArgs e )
+    {
+      if (isTaskAndDateValid())
+      {
+        AddTask( txtTask.Text, myTasks );
+      }
+    }
+
+    private bool isTaskAndDateValid()
+    {
+
+      if(txtTask.Text == "")
+      {
+        MessageBox.Show( "You must enter a task", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Error );
+        return false;
+      }
+
+
+      if (myTasks.ContainsKey( dtpTaskDate.Value.ToShortDateString()))
+      {
+        MessageBox.Show( "Only one task per date is allowed", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Error );
+        return false;
+      }
+
+      return true;
+    }
+
+
+    private void btnRemoveTask_Click( object sender, EventArgs e )
+    {
+      if(lstTasks.SelectedIndex > -1)
+      {
+        RemoveTask( lstTasks.SelectedItem.ToString() );
+      }
+      else
+      {
+        MessageBox.Show( "Please select an item", "Abort Operation", MessageBoxButtons.OK, MessageBoxIcon.Error );
+      }
+    }
+
+    private void  PrintItems()
+    {
+
+      string msg = "";
+      foreach(KeyValuePair<string, string> task in myTasks)
+      {
+        msg += $"{task.Key} {task.Value} \n";
+      }
+
+      MessageBox.Show( msg );
+    }
+
+    private void btnPrintAll_Click( object sender, EventArgs e )
+    {
+      if(lstTasks.Items.Count > 0)
+      {
+        PrintItems();
+      }
+      else
+      {
+        MessageBox.Show( "No items to print", "Abort operation", MessageBoxButtons.OK, MessageBoxIcon.Error );
+      }
+
+    }
+
+    private void lstTasks_SelectedValueChanged( object sender, EventArgs e )
+    {
+      TaskSelectedDisplay();
+    }
+  }
 }
